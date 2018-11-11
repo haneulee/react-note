c0c768654c7a1ecd290571a440390ffc1d0032b5
Haneul Lee <lovesky4294@gmail.com>
2018-11-12 01:30:36 +0900

    add note mutation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-// import { Query } from "react-apollo";
-// import { GET_NOTES } from "./queries";
+import { Query } from "react-apollo";
+import { GET_NOTES } from "./queries";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Query query={GET_NOTES}>{() => null}</Query> */}
+        <Query query={GET_NOTES}>{() => null}</Query>
       </div>
     );
   }
--- src/clientState.js
+++ src/clientState.js
@@ -1,7 +1,8 @@
 import { NOTE_FRAGMENT } from "./fragments";
+import { GET_NOTES } from "./queries";
 
 export const defaults = {
-  note: [
+  notes: [
     {
       __typename: "Note",
       id: 1,
@@ -12,24 +13,24 @@ export const defaults = {
 };
 export const typeDefs = [
   `
-    schema {
-        query: Query
-        mutation: Mutation
-    }
-    type Query {
-        notes: [Note]!
-        note(id: Int!): Note
-    }
-    type Mutation {
-        createNote(title: String!, content: String!): Note
-        editNote(id: Int!, title: String!, content: String!): Note
-    }
-    type Note {
-        id: Int!
-        title: String!
-        content: String!
-    }
-`
+      schema {
+          query: Query
+          mutation: Mutation
+      }
+      type Query {
+          notes: [Note]!
+          note(id: Int!): Note
+      }
+      type Mutation {
+          createNote(title: String!, content: String!): Note
+          editNote(id: Int!, title: String!, content:String!): Note
+      }
+      type Note {
+          id: Int!
+          title: String!
+          content: String!
+      }
+      `
 ];
 export const resolvers = {
   Query: {
@@ -38,9 +39,26 @@ export const resolvers = {
         __typename: "Note",
         id: variables.id
       });
-
       const note = cache.readFragment({ fragment: NOTE_FRAGMENT, id });
       return note;
+    }
+  },
+  Mutation: {
+    createNote: (_, variables, { cache }) => {
+      const { notes } = cache.readQuery({ query: GET_NOTES });
+      const { title, content } = variables;
+      const newNote = {
+        __typename: "Note",
+        title,
+        content,
+        id: notes.length + 1
+      };
+      cache.writeData({
+        data: {
+          notes: [newNote, ...notes]
+        }
+      });
+      return newNote;
     }
   }
 };
